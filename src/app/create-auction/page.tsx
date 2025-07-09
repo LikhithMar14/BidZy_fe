c0291e980203e42f6689f73d2231932bcbf52f35
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-09 16:38:44 +0530

    commit

--- src/app/create-auction/page.tsx
+++ src/app/create-auction/page.tsx
@@ -169,10 +169,10 @@ export default function CreateAuctionPage() {
   const onSubmit = async (data: CreateAuctionForm) => {
     if (!user?.id) {
       toast.error("Please log in to create an auction");
-      if(user.email !== "likhithcvsrl@gmail.com"){
-        toast.warning("Only admin can create the auction")
-      }
       return;
+    }
+    if(user.email !== "likhithcvsrl@gmail.com"){
+      toast.warning("Only admin can create the auction")
     }
 
     if (!selectedFile) {
